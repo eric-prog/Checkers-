d429132b94a1648466e3b04415aefe70a281f61d
eric-prog <eric.sprog@gmail.com>
2023-01-08 01:51:46 -0500

    Added a home screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,20 @@ import { CheckersModel } from './models/CheckersModel'
 
 
 function App() {
-  const [winner, setWinner] = useState("");
+  const [winner, setWinner] = useState("")
+  const [mode, setMode] = useState("")
 
   var checkersBoard = new CheckersModel()
   var board = new BoardModel(checkersBoard.createBoard(), 12, 0)
 
   function resetGame() {
-    checkersBoard = new CheckersModel()
-    board = new BoardModel(checkersBoard.createBoard(), 12, 0)
+    setMode("")
     setWinner("")
+  }
+
+  function faceHuman(mode: string) {
+    setMode(mode)
+    board = new BoardModel(checkersBoard.createBoard(), 12, 12)
   }
 
   useEffect(() => {
@@ -29,7 +34,17 @@ function App() {
             <button className='reset-btn' onClick={resetGame}>Reset Game</button>
         </div> : null
       }
-      <Board board={board} setWinner={setWinner} />
+      {
+        mode === "" ? 
+        <div className='game-screen'>
+          <h1 className="winner-title game-title">Welcome to Checkers!</h1>
+          <div className='game-options'>
+            <button className='reset-btn' onClick={() => {faceHuman("Human")}}>Human vs Human</button>
+            <button className='reset-btn' onClick={() => {faceHuman("Computer")}}>Computer vs Human</button>
+          </div>
+        </div>
+        : <Board board={board} setWinner={setWinner} /> 
+      }
     </div>
   )
 }
